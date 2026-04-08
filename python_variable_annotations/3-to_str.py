7d5674289fa49c29f458fc3d392b3ee19aa3b38a
DaRKkem <damross446@gmail.com>
2026-04-08 13:16:12 +0200

    finished

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for converting a float to its string representation."""
+
+import math
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation."""
+    return str(n)
