fd82f8ba71a13068de7c4f1ea2c6fe091ab84f6d
DaRKkem <damross446@gmail.com>
2026-04-21 20:12:12 +0200

    finished

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,5 @@
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of start and end indexes for pagination."""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
