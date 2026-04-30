e201ede0990581c798dcac547056472f58ad4e2f
DaRKkem <damross446@gmail.com>
2026-04-30 09:35:28 +0200

    finished

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,6 +1,5 @@
-// Replace var by const
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // var → const
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,9 +7,9 @@ export function getLast() {
   return ' is okay';
 }
 
-// Replace var by let
 export function taskNext() {
-  let combination = 'But sometimes let'; // var → let
+  let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
