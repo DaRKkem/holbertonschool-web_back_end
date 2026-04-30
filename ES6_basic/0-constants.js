0a81d6e1d85b30c111210b15cf1b2b25a6cc6088
DaRKkem <damross446@gmail.com>
2026-04-30 09:34:16 +0200

    finished

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,10 +1,14 @@
-// Remplacer var par const ici
+// Replace var by const
 export function taskFirst() {
   const task = 'I prefer const when I can.'; // var → const
   return task;
 }
 
-// Remplacer var par let ici
+export function getLast() {
+  return ' is okay';
+}
+
+// Replace var by let
 export function taskNext() {
   let combination = 'But sometimes let'; // var → let
   combination += getLast();
