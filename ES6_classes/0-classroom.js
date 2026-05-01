ce241a4388712aca6b1124f697375eb71af1f51c
DaRKkem <damross446@gmail.com>
2026-05-01 23:48:20 +0200

    finished

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,12 +1,5 @@
-// Remplacer var par const ici
-export function taskFirst() {
-  const task = 'I prefer const when I can.'; // var → const
-  return task;
-}
-
-// Remplacer var par let ici
-export function taskNext() {
-  let combination = 'But sometimes let'; // var → let
-  combination += getLast();
-  return combination;
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
