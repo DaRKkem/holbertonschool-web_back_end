27319817b14533471a48ed3b16c4bebbe2e6432d
DaRKkem <damross446@gmail.com>
2026-05-05 09:56:46 +0200

    finished

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,6 @@
+// Returns an array of ids from a list of objects
+export default function getListStudentsIds(arr){
+    if (!Array.isArray(arr))
+        return [];
+    return arr.map(element => element.id);
+}
