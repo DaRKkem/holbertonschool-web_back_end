63b13430a1304e31a27bb9fef3b035bee41f25da
DaRKkem <damross446@gmail.com>
2026-05-06 20:02:22 +0200

    finished

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+// Handle a response from an API
+export default function handleResponseFromAPI(promise) {
+    return promise
+        .then(() => ({ status: 200, body: 'success' }))
+        .catch(() => new Error())
+        .finally(() => console.log('Got a response from the API'));
+}
