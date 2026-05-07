f007a59538b9a05e34ebf1cec547ce4b510d3f79
DaRKkem <damross446@gmail.com>
2026-05-07 14:25:44 +0200

    finished

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
         .then((results) => results.map((result) => ({
             status: result.status,
-            value: result.value || result.reason,
+            value: result.value || result.reason.toString(),
         })));
 }
 
