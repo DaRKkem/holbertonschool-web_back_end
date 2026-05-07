c5622de322a1e45761573f4b2f16aa0083c1cb11
DaRKkem <damross446@gmail.com>
2026-05-07 13:50:19 +0200

    finished

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+// Handles multiple promises and returns an array of their statuses and values
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+        .then((results) => results.map((result) => ({
+            status: result.status,
+            value: result.value || result.reason,
+        })));
+}
