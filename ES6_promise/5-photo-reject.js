05e64cd2e422dba486bd4dbbb02d72c1a2d8f17d
DaRKkem <damross446@gmail.com>
2026-05-07 14:16:45 +0200

    finished

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 // Returns a promise that rejects with an error message for the given filename
-export default function uploadPhoto(filename) {
+export default function uploadPhoto(fileName) {
     return new Promise((resolve, reject) => {
         reject(new Error(`${fileName} cannot be processed`))
     });
