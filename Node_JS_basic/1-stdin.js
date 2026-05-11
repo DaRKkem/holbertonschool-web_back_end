28dd7362da78fa82eaa670b550bbd4a0cba417e2
DaRKkem <damross446@gmail.com>
2026-05-11 11:01:46 +0200

    finished

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,5 +4,5 @@ process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data}`);
 });
 process.stdin.on('close', () => {
-    process.stdout.write(`This important software is now closing`);
+    process.stdout.write("This important software is now closing\n");
 })
