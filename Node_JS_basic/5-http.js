31cf8eabc88ccce17ad449b9d6ae439db463b08a
DaRKkem <damross446@gmail.com>
2026-05-12 20:39:00 +0200

    finished

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -11,18 +11,14 @@ function countStudents (path) {
       const lines = data.split('\n').filter((line) => line !== '');
       const students = lines.slice(1);
       // difference with task 3 below (result)
-      let result = `Number of students: ${students.length}\n`;
+      let result = `Number of students: ${students.length}`;
 
-      const fields = {};
-      students.forEach((line) => {
-        const [firstname, , , field] = line.split(',');
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstname);
+      const lines_result = [];
+      Object.keys(fields).forEach((field) => {
+        lines_result.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       });
 
-      Object.keys(fields).forEach((field) => { // difference with task 3 below (result)
-        result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-      });
+      result += '\n' + lines_result.join('\n');
       return resolve(result);
     });
   });
