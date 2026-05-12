838ea0ce28c796fcd8ef192359c914c6532df4a9
DaRKkem <damross446@gmail.com>
2026-05-12 20:46:59 +0200

    finished

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -31,7 +31,7 @@ function countStudents (path) {
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(process.argv[2])
       .then((data) => res.end(`This is the list of our students\n${data}`))
